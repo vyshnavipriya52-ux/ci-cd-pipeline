315e7d6a1d400772f2bab78ba6f3408dd2b5fafa
vyshnavipriya52-ux <vyshnavipriya52@gmail.com>
2026-03-24 06:22:09 +0000

    added frontend UI with HTML & CSS

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+// Serve static files (CSS)
+app.use(express.static(__dirname));
+
+// Send HTML file
 app.get('/', (req, res) => {
-  res.send("CI/CD Multi Env 🚀");
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log("Server running"));
